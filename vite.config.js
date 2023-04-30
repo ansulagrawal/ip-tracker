0244a595ae071d3d75aaac3d06f9ab5b78ad7d1c
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-04-30 16:01:57 +0530

    Added Base Url

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({ plugins: [react()], server: { port: 8080 }, preview: { port: 8080 }, base: '/ip-tracker/' });
